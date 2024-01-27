caa4c0306433476ffcd5a582fc260984166d2b9c
Beth <minthu.mingalarit@gmail.com>
2024-01-27 23:01:37 +0630

    route and gurad  setup

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-import './assets/base.css'
-import './assets/main.css'
+import './assets/base.css';
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
-import VeeValidatePlugin from './includes/validation' // validation plugin add
-import { auth } from './includes/firebase'
+import App from './App.vue';
+import router from './router';
+import VeeValidatePlugin from './includes/validation'; // validation plugin add
+import { auth } from './includes/firebase';
 
-let app
+let app;
 
 auth.onAuthStateChanged(() => {
   if (!app) {
-    app = createApp(App)
+    app = createApp(App);
 
-    app.use(createPinia())
-    app.use(router)
-    app.use(VeeValidatePlugin) // validation plugin use
+    app.use(createPinia());
+    app.use(router);
+    app.use(VeeValidatePlugin); // validation plugin use
 
-    app.mount('#app')
+    app.mount('#app');
   }
-})
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/HomeView.vue';
+import About from '../views/AboutView.vue';
+import Manage from '../views/Manage.vue';
+import useUserStore from '../stores/user';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('manage route gurard');
+      next();
+    },
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+});
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta);
+  if (!to.meta.requiresAuth) {
+    return next();
+  }
+  const store = useUserStore();
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
+});
 
-export default router
+export default router;
