fa58521a37891361310eb9a8bd545cf80f4c299b
Beth <minthu.mingalarit@gmail.com>
2024-01-31 01:11:43 +0630

    CURD in firebase

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,18 +1,22 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
-import 'firebase/compat/firestore'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBuatmJmNTGiqOiVLg2wTVvsfZVlIrmYqQ',
   authDomain: 'music-ed0d8.firebaseapp.com',
   projectId: 'music-ed0d8',
   storageBucket: 'music-ed0d8.appspot.com',
   appId: '1:167190151142:web:89bb5a949e5d86f2562027'
-}
+};
 
-export default firebase.initializeApp(firebaseConfig)
+export default firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth()
-const db = firebase.firestore()
+const auth = firebase.auth(); // call auth from firebase
+const db = firebase.firestore(); // call firestore from firebase
+const storage = firebase.storage(); // call storage from firebase
 
-const usersCollection = db.collection('users')
-export { auth, db, usersCollection }
+const usersCollection = db.collection('users');
+const songsCollection = db.collection('songs');
+export { auth, db, usersCollection, songsCollection, storage };
